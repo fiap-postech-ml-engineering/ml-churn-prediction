69403a68ef8b0f30cceb2e67fcba9b73f0fda4ea
Akira <vitorucha96@gmail.com>
2026-04-20 16:45:04 -0300

    feat(models): extrair lógica de modelos para src/models

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,21 @@
 """Models package for churn prediction."""
 
+from .baseline_models import (
+    build_dummy_classifier,
+    build_logistic_regression,
+    log_baseline_run_to_mlflow,
+    train_and_evaluate_baseline,
+)
+from .evaluate import compute_classification_metrics
 from .mlp_model import MLPNetworkChurn
+from .train import train_mlp_model
 
-__all__ = ["MLPNetworkChurn"]
+__all__ = [
+    "MLPNetworkChurn",
+    "compute_classification_metrics",
+    "train_mlp_model",
+    "build_dummy_classifier",
+    "build_logistic_regression",
+    "train_and_evaluate_baseline",
+    "log_baseline_run_to_mlflow",
+]
